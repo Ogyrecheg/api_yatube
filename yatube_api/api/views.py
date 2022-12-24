52b890218f90cb156112e34171b63ad4ae649a6d
Шевченко Александр <ogyrecheg@yandex.ru>
2022-12-25 00:17:21 +0300

    pdz ya typanyl s permissions

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import status, viewsets
-from rest_framework.exceptions import PermissionDenied
-from rest_framework.response import Response
+from posts.models import Group, Post, User
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-from posts.models import Comment, Group, Post, User
-
+from .permissions import IsOwnerOrReadOnly
 from .serializers import (CommentSerializer, GroupSerializer, PostSerializer,
                           UserSerializer)
 
@@ -12,27 +11,10 @@ from .serializers import (CommentSerializer, GroupSerializer, PostSerializer,
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly, )
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        super(PostViewSet, self).perform_update(serializer)
-
-    def partial_update(self, request, *args, **kwargs):
-        kwargs['partial'] = True
-        return self.update(request, *args, **kwargs)
-
-    def destroy(self, request, pk=None):
-        post = get_object_or_404(Post, id=pk)
-        if request.user != post.author:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-
-        post.delete()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -47,6 +29,7 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
 
     def get_queryset(self):
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
@@ -57,18 +40,3 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
         serializer.save(author=self.request.user, post=post)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        super(CommentViewSet, self).perform_update(serializer)
-
-    def destroy(self, request, post_id=None, pk=None):
-        post = get_object_or_404(Post, id=post_id)
-        comment = post.comments.get(id=pk)
-        if request.user != comment.author:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-
-        comment.delete()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
